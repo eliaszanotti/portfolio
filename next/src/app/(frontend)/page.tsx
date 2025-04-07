6f25971c3c0923308d1e3c263eebbef8f0d2e221
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-04-07 23:43:32 +0200

    add test

--- next/src/app/(frontend)/page.tsx
+++ next/src/app/(frontend)/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
 	return (
-		<>
-			<h1>Hello World</h1>
-		</>
+		<section className="flex h-screen w-full flex-col items-center justify-center gap-4">
+			<h1 className="text-4xl font-bold">Hello World</h1>
+			<p>This is a test</p>
+			<button className="btn btn-primary">DaisyUI Button</button>
+		</section>
 	);
 }
