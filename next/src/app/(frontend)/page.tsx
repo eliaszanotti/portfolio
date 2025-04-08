a5ae88b5bff2557898aed51dbb2b1913867c48a3
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-04-08 12:06:08 +0200

    add theme

--- next/src/app/(frontend)/page.tsx
+++ next/src/app/(frontend)/page.tsx
@@ -1,9 +1,15 @@
+import Man from "@icons/man";
+
 export default function Home() {
 	return (
-		<section className="flex h-screen w-full flex-col items-center justify-center gap-4">
-			<h1 className="text-4xl font-bold">Hello World</h1>
-			<p>This is a test</p>
-			<button className="btn btn-primary">DaisyUI Button</button>
+		<section className="relative w-full h-full">
+			<div className="absolute z-0 w-full h-full grid place-items-center">
+				<Man />
+			</div>
+			<div className="relative z-10 flex h-screen w-full flex-col items-center justify-center gap-4">
+				<h1 className="text-9xl font-black">ELIAS</h1>
+				<h1 className="text-9xl font-black">ZANOTTI</h1>
+			</div>
 		</section>
 	);
 }
