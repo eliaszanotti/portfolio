67a05e2287a13726626e9d522451b62b274e0c9c
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-04-17 15:15:32 +0200

    achange theme

--- next/src/app/(frontend)/page.tsx
+++ next/src/app/(frontend)/page.tsx
@@ -6,17 +6,17 @@ export default function Home() {
 		<section className="relative w-full h-screen grid place-items-center">
 			<div className="absolute p-20 w-full h-full grid grid-cols-[1fr_auto]">
 				<IconFrance />
-				<p className="text-2xl">FULL STACK DEVELOPER</p>
+				<p className="text-2xl text-primary">FULL STACK DEVELOPER</p>
 			</div>
 			<div className="p-10 grid grid-cols-[1fr_1fr] gap-10">
 				<div className="h-full w-fit">
 					<Man />
 				</div>
 				<div className="flex flex-col items-center justify-center">
-					<h1 className="text-[200px] leading-none font-black">
+					<h1 className="text-[200px] leading-none font-black text-primary">
 						ELIAS
 					</h1>
-					<h1 className="text-[130px] leading-none font-black">
+					<h1 className="text-[130px] leading-none font-black text-secondary">
 						ZANOTTI
 					</h1>
 				</div>
