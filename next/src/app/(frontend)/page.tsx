b27619bddb88d0b9546273d47406b0f265580c08
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-04-17 16:13:40 +0200

    fix home page

--- next/src/app/(frontend)/page.tsx
+++ next/src/app/(frontend)/page.tsx
@@ -1,26 +1,12 @@
 import IconFrance from "@/icons/france";
 import Man from "@/icons/man";
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
 	return (
 		<div className="flex flex-col">
-			<section className="relative w-full h-screen grid place-items-center">
-				<div className="absolute p-20 w-full h-full grid grid-cols-[1fr_auto]">
-					<IconFrance />
-					<p className="text-2xl text-primary">
-						FULL STACK DEVELOPER
-					</p>
-				</div>
-				<div className="p-10 grid place-items-center gap-10 relative w-full h-full">
-					<div className="flex flex-col items-center justify-center fixed z-10">
-						<h1 className="text-[300px] leading-none font-black text-primary">
-							ELIAS
-						</h1>
-						<h1 className="text-[200px] leading-none font-black text-primary">
-							ZANOTTI
-						</h1>
-					</div>
+			<section className="relative w-full h-screen">
+				<div className="absolute inset-0 p-10 grid place-items-center gap-10 w-full h-full">
 					<div className="absolute inset-0 grid place-items-center w-full h-full">
 						<Image
 							className="h-2/3 w-full object-contain relative"
@@ -30,9 +16,45 @@ export default function Home() {
 							height={300}
 						/>
 					</div>
+					<div className="flex flex-col items-center justify-center fixed">
+						<h1 className="text-[300px] leading-none font-black text-primary">
+							ELIAS
+						</h1>
+						<h1 className="text-[200px] leading-none font-black text-primary">
+							ZANOTTI
+						</h1>
+					</div>
+				</div>
+				<div className="w-full h-48 grid items-center grid-cols-[1fr_auto] px-16 fixed top-0 z-50">
+					<nav className="flex items-center gap-4">
+						<Link href="/">
+							<IconFrance />
+						</Link>
+						<Link
+							className="btn btn-neutral btn-ghost font-normal uppercase"
+							href="/"
+						>
+							Home
+						</Link>
+						<Link
+							className="btn btn-neutral btn-ghost font-normal uppercase"
+							href="/about"
+						>
+							About
+						</Link>
+						<Link
+							className="btn btn-neutral btn-ghost font-normal uppercase"
+							href="/contact"
+						>
+							Contact
+						</Link>
+					</nav>
+					<p className="text-2xl text-primary">
+						FULL STACK DEVELOPER
+					</p>
 				</div>
 			</section>
-			<section className="h-screen grid place-items-center z-50 bg-base-200">
+			<section className="h-screen grid place-items-center z-5 bg-base-200 text-primary">
 				<p className="text-6xl font-black">Sortez du cadre</p>
 			</section>
 		</div>
