e2ee2f560dbffbd31d2671f56dce66f7bc5332f1
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-05-08 16:45:32 +0200

    add optimize next

--- next/next.config.ts
+++ next/next.config.ts
@@ -2,8 +2,18 @@ import type { NextConfig } from "next";
 import { withPayload } from "@payloadcms/next/withPayload";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	// output: "standalone",
+	output: "standalone",
+	poweredByHeader: false,
+	compress: true,
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		unoptimized: true, // Désactive l'optimisation d'images pendant le build
+	},
+	experimental: {
+		optimizeCss: true,
+		optimizePackageImports: ["@payloadcms/next", "payload"],
+	},
 };
 
 export default withPayload(nextConfig);
