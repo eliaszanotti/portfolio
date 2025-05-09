a2e9d5e2622961cde46d575279df84674656bc73
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-05-09 21:48:39 +0200

    add footer fix bg color

--- next/src/components/Footer.tsx
+++ next/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer className="bg-base-100 p-4">
+		<footer className="bg-base-200 p-4">
 			<p>Footer, Site by Elias Zanotti</p>
 		</footer>
 	);
