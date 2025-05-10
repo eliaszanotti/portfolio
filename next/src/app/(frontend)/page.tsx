cfa1952cc3f39471ba812e01b9dd7ec875847365
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-05-10 12:46:34 +0200

    fix

--- next/src/app/(frontend)/page.tsx
+++ next/src/app/(frontend)/page.tsx
@@ -82,13 +82,26 @@ export default function Home() {
 			</section>
 			<section
 				ref={addSectionRef}
-				className="h-screen grid place-items-center z-5 bg-base-200 text-primary"
+				className="min-h-screen grid place-items-center bg-base-200 p-16"
 			>
-				<div className="flex flex-col gap-4">
-					<h1 className="text-6xl font-black italic">
-						&quot;Think outside the box&quot;
-					</h1>
-					<p className="font-bold">Welcome to Elias&apos;s museum</p>
+				<div className="max-w-6xl grid grid-cols-2 gap-8">
+					<div className="flex flex-col gap-4 justify-center">
+						<h1 className="text-6xl font-black italic">
+							&quot;Think outside the box&quot;
+						</h1>
+						<p className="font-bold">
+							Welcome to Elias&apos;s museum
+						</p>
+					</div>
+					<div className="">
+						<Image
+							className="h-1/2 object-contain"
+							src="/images/frame-1.png"
+							alt="elias"
+							width={1000}
+							height={1000}
+						/>
+					</div>
 				</div>
 			</section>
 			<SectionFirst addSectionRef={addSectionRef} />
