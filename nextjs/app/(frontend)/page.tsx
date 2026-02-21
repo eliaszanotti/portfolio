91402ee56e5a4d05048d81550c9a5d3063c95cae
Elias ZANOTTI <zanotti.elias@gmail.com>
2026-02-21 12:34:52 +0100

    fix tailwind

--- nextjs/app/(frontend)/page.tsx
+++ nextjs/app/(frontend)/page.tsx
@@ -1,12 +1,10 @@
 import { headers as getHeaders } from "next/headers.js";
 import Image from "next/image";
 import { getPayload } from "payload";
-import React from "react";
 import { fileURLToPath } from "url";
 
 import { Button } from "components/ui/button";
 import config from "payload.config";
-import "../globals.css";
 
 export default async function HomePage() {
 	const headers = await getHeaders();
