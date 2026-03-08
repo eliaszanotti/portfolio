ce4b4c66956d16638c60baf0162c1ef44e7b96b2
Elias ZANOTTI <zanotti.elias@gmail.com>
2026-03-08 18:08:03 +0100

    add suspense

--- nextjs/app/[locale]/(frontend)/page.tsx
+++ nextjs/app/[locale]/(frontend)/page.tsx
@@ -4,16 +4,30 @@ import { ExperienceSection } from "@/components/experience";
 import { HomeSection } from "@/components/home";
 import { ProjectsSection } from "@/components/projects";
 import { SkillsSection } from "@/components/skills";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Suspense } from "react";
 
 export default function HomePage() {
 	return (
 		<div className="container mx-auto space-y-16 p-4 overflow-hidden">
-			<HomeSection />
-			<SkillsSection />
-			<ProjectsSection />
-			<ExperienceSection />
-			<AboutSection />
-			<ContactSection />
+			<Suspense fallback={<Skeleton />}>
+				<HomeSection />
+			</Suspense>
+			<Suspense fallback={<Skeleton />}>
+				<SkillsSection />
+			</Suspense>
+			<Suspense fallback={<Skeleton />}>
+				<ProjectsSection />
+			</Suspense>
+			<Suspense fallback={<Skeleton />}>
+				<ExperienceSection />
+			</Suspense>
+			<Suspense fallback={<Skeleton />}>
+				<AboutSection />
+			</Suspense>
+			<Suspense fallback={<Skeleton />}>
+				<ContactSection />
+			</Suspense>
 		</div>
 	);
 }
